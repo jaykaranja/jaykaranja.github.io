9ea10ea4e19eaeafdbcc7a6c8d0e177a69b98bf6
James <mainakaranja2001@gmail.com>
2023-01-09 15:49:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 let hidden = true;
 $("button.burger").click(function(){
     if(hidden == true){
-        $(".navbar-menu").animate({marginLeft: '250px'}, "slow");
+        $(".navbar-menu").animate({marginLeft: '200px'}, "slow");
         hidden = false;
     }
     else {
